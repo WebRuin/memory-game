74efb31f1efe75d2bb098bd36da0e0726774ae7a
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2022-08-07 13:19:03 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,63 @@
+<script>
+  import Card from "./lib/Card.svelte";
+
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  const icons = [
+    "🍺",
+    "🍺",
+    "👽",
+    "👽",
+    "🦄",
+    "🦄",
+    "🍔",
+    "🍔",
+    "🪨",
+    "🪨",
+    "🦎",
+    "🦎",
+    "🤬",
+    "🤬",
+    "🔥",
+    "🔥",
+  ];
+
+  const shuffledIcons = shuffle(icons);
+</script>
+
+<main>
+  <div class="grid">
+    {#each shuffledIcons as icon}
+      <Card {icon} />
+    {/each}
+  </div>
+</main>
+
+<style>
+  .grid {
+    width: 800px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    justify-items: center;
+    align-items: center;
+  }
+</style>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Flip from "../lib/Flip.svelte";
+  import bg from "../assets/bg.svg";
+
+  let flip = false;
+
+  export let icon;
+
+  function handleClick() {
+    flip = !flip;
+  }
+</script>
+
+<div on:click={handleClick} class="card">
+  <Flip height="200px" width="200px" {flip}>
+    <img slot="front" src={bg} alt="card back" />
+    <p slot="back">{icon}</p>
+  </Flip>
+</div>
+
+<style>
+  p {
+    margin: 0;
+    padding: 0;
+    font-size: 4rem;
+    background: #efe;
+    text-shadow: 1px 1px 2px black;
+  }
+  p,
+  img {
+    width: 200px;
+    height: 200px;
+    display: grid;
+    place-items: center;
+    border-radius: 20px;
+    border: 6px solid transparent;
+  }
+</style>
